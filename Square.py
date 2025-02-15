81f79e0e59d3410365b506e0cf27229ba9dfee0e
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2025-02-15 06:29:52 +0300

    Update Square.py

--- Square.py
+++ Square.py
@@ -3,15 +3,15 @@ lines=[line for line in f]
 b=[0]*10
 c=[0]*10
 for j in range(1,len(lines)):
-    b[j-1]=float(lines[j][0:5])
-    c[j-1]=float(lines[j][6:10])
+    b[j-1]=float(lines[j].split(',')[0])
+    c[j-1]=float(lines[j].split(',')[1])
 d=3.14
 while True:
     def error(weight):
         err=0.0
         for i in range(10):
             err+=(b[i]*weight-c[i])**2
-            return err
+        return err
     if error(d+0.01)<error(d):
         d=d+0.01
     elif error(d-0.01)<error(d):
