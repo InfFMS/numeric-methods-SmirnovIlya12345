064305c95cdcb77df42dd44173f24b986d43fd61
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2025-02-15 06:54:55 +0300

    Update Square.py

--- Square.py
+++ Square.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 f=open('data.csv', 'r', encoding='utf-8')
 lines=[line for line in f]
 b=[0]*10
@@ -5,6 +7,7 @@ c=[0]*10
 for j in range(1,len(lines)):
     b[j-1]=float(lines[j].split(',')[0])
     c[j-1]=float(lines[j].split(',')[1])
+print(b,c)
 d=3.14
 while True:
     def error(weight):
@@ -19,3 +22,9 @@ while True:
     else:
         print('Resistance is', round(d,2))
         break
+fig,ax=plt.subplots()
+plt.scatter(b,c)
+x=[0,3]
+y=[0,3*d]
+plt.plot(x, y, color="blue")
+plt.show()
