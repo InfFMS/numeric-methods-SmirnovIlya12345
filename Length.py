b493b73a115b51f23d6553d21f4afb6fd03a76a3
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2025-02-20 23:02:01 +0700

    Update Length.py

--- Length.py
+++ Length.py
@@ -1,11 +1,13 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+# y=0 - линия раздела фаз
 print('Enter the coordinates of the starting point and the ending point')
 x1=float(input())
 y1=float(input())
 x2=float(input())
 y2=float(input())
+# Частное скоростей света
 print('Enter the optic density of the second material relative to the first material')
 n=float(input())
 where1=x1
@@ -22,6 +24,7 @@ while True:
         where=where1*0.5+where2*0.5
         break
 print(where)
+print('Angle:', np.atan(y1/where))
 xfirst=np.linspace(x1,where,2)
 xsecond=np.linspace(where,x2,2)
 xgeneral=np.linspace(x1,x2,2)
